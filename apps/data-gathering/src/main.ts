db3f119b298731a3300614a874785e43fd7ece1f
Yud Bet <66458858+Yud-Bet@users.noreply.github.com>
2023-01-30 23:22:49 +0700

    Enable cors

--- apps/data-gathering/src/main.ts
+++ apps/data-gathering/src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { DataGatheringModule } from './data-gathering.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(DataGatheringModule);
+  const app = await NestFactory.create(DataGatheringModule, { cors: true });
   await app.listen(3000);
 }
 bootstrap();
--- apps/query/src/main.ts
+++ apps/query/src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { QueryModule } from './query.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(QueryModule);
+  const app = await NestFactory.create(QueryModule, { cors: true });
   await app.listen(3000);
 }
 bootstrap();
